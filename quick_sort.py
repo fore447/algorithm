b008a0c48138111a126a5f74338032b30cb7da6e
fore447 <mmori.0426@gmail.com>
2022-01-13 23:19:45 +0900

    create file

---
+++ quick_sort.py
@@ -0,0 +1,26 @@
+import py_compile
+
+
+data = [6, 15, 4, 2, 8, 5, 11, 9, 7, 13]
+
+def quick_sort(data):
+    if len(data) <= 1:
+        return data
+    
+    pivot = data[0]
+    left, right, same = [], [], 0
+
+    for i in data:
+        if i < pivot:
+            left.append(i)
+        if i > pivot:
+            right.append(i)
+    else:
+        same += 1
+
+    left = quick_sort(left)
+    right = quick_sort(right)
+    # ソートされたものとピボットの値を合わせて返す
+    return left + [pivot] * same + right
+    
+print(quick_sort(data))
